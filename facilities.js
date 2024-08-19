b596d6cdd8056be6126af3da60a15a16e1d6474a
Oluwadunsin david <oluwadunsindavid21@gmail.com>
2024-08-19 16:50:10 +0100

    src

--- facilities.js
+++ facilities.js
@@ -1,11 +1,11 @@
 // SCROLL REVEAL
 
-let sr = ScrollReveal({
+let src = ScrollReveal({
     duration: 2500,
     distance: "60px",
 });
 
-sr.reveal(".h2", {origin: "top", delay: 500});
-sr.reveal(".p", {origin: "top", delay: 600});
-sr.reveal("#faci-img1", {origin: "left", delay: 600} );
-sr.reveal("#faci-img2", {origin: "right", delay: 600} );
+src.reveal(".h2", {origin: "top", delay: 500});
+src.reveal(".p", {origin: "top", delay: 600});
+src.reveal("#faci-img1", {origin: "left", delay: 600} );
+src.reveal("#faci-img2", {origin: "right", delay: 600} );
